52e3dde446a37405d7f5985f5d4c740015fa96ab
valholic <yansenlodova@gmail.com>
2024-11-08 08:43:14 +0700

    change port in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let multer = require('multer');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Router
 const menuRoutes = require('./src/routes/menu');
